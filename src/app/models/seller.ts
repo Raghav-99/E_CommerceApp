9628f79e5e098e0a086be70b6a4536b49c4cf25d
Mohit Kumar Sinha <mohitsin@cybage.com>
2022-11-26 22:01:42 +0530

    Seller reg page validation and service ready for testing

--- src/app/models/seller.ts
+++ src/app/models/seller.ts
@@ -6,23 +6,20 @@ export class Seller{
     answer:string='';
     phoneNo:string='';
     shopName:string='';
+    address:string='';
     shopRegNo:string='';
-    productName:string='';
-    productDetails:string='';
 
-   
-    //active:number=1;
-    // constructor(fullname:string='', Username:string='',email:string='',secQuestion:string='',Answer:string='',Phoneno:string,
-    // Shopname:string='',Shopno:string='',productDetails:string=''){
-    //     this.fullname=fullname;
-    //     this.Username=Username
-    //     this.email=email;
-    //     this.secQuestion=secQuestion;
-    //     this.Answer=Answer;
-    //     this.Phoneno=Phoneno;
-    //     this.Shopname=Shopname;
-    //     this.Shopno=Shopno;
-    //     this.productDetails=productDetails;
+    constructor(fullname?:string, Username?:string,email?:string,secQuestion?:string,Answer?:string,Phoneno?:string,
+    Shopname?:string,Shopno?:string,address?:string){
+        this.fullName=fullname||'';
+        this.username=Username||'';
+        this.email=email||'';
+        this.securityQuestion=secQuestion||'';
+        this.answer=Answer||'';
+        this.phoneNo=Phoneno||'';
+        this.address=address||'';
+        this.shopName=Shopname||'';
+        this.shopRegNo=Shopno||'';
 
-    // }
+    }
 }
